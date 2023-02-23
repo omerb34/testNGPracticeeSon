f7439e2de10ee4e881eae67620c00f6c89fce9ed
Erol Evren <100449806+erlevren@users.noreply.github.com>
2023-02-23 19:08:36 +0300

    23/02/23

---
+++ src/test/java/tests/Practice01.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class Practice01 {
+    /*
+    --http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html adresine gidiniz
+    --Tüm ülkeleri yazdırınız
+    --Ülkeler ile şehirleri eşleştiriniz
+     */
+}
---
+++ src/test/java/tests/Practice02.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class Practice02 {
+    /*
+    Negative Test
+    1.  https://id.heroku.com/login sayfasina gidin
+    2.  Data provider kullanarak mail adres ve passwordleri giriniz
+    3.  Login butonuna tiklayiniz
+    4.  "There was a problem with your login." texti gorunur olduğunu test edin
+    5.  sayfalari kapatiniz
+
+ */
+}
